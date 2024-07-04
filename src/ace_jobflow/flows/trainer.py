0b4e276ee67d0f5a974ae379b850fa2af10e885d
Vir Karan <virkaran123456@gmail.com>
2024-07-04 15:19:02 -0700

    fix atomate2 import

--- src/ace_jobflow/flows/trainer.py
+++ src/ace_jobflow/flows/trainer.py
@@ -4,7 +4,7 @@ from ace_jobflow.jobs.data import read_outputs
 from ace_jobflow.jobs.train import naive_train_ACE
 
 
-def naive_flow(compositions: list, num_points: int = 5, temperature: float = 2000, max_steps: int = 2000, batch_size: int = 200, gpu_index: int | None = None):
+def naive_flow(compositions: list, num_points: int = 5, temperature: float = 2000, max_steps: int = 2000, batch_size: int = 200, gpu_index: int = None):
     data = data_gen_flow(compositions, num_points=num_points, temperature=temperature)
     read_job = read_outputs(data.output)
     trainer = naive_flow(read_job.output, max_steps=max_steps, batch_size=batch_size, gpu_index=gpu_index)
