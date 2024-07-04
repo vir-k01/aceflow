58f273ed85ab2b049a3a77a672c188242dfff5b2
Vir Karan <virkaran123456@gmail.com>
2024-07-04 15:22:00 -0700

    check MLMD for test

--- src/ace_jobflow/jobs/data.py
+++ src/ace_jobflow/jobs/data.py
@@ -9,8 +9,8 @@ def read_outputs(md_outputs: List, step_skip: int= 1):
     forces = []
     structures = []
     for md_output in md_outputs:
-        trajectory = md_output.vasp_objects['trajectory']
-        #trajectory = md_output.forcefield_objects['trajectory']
+        #trajectory = md_output.vasp_objects['trajectory']
+        trajectory = md_output.forcefield_objects['trajectory']
         for frame_id in range(0, len(trajectory.frame_properties), step_skip):
             energies.append(trajectory.frame_properties[frame_id]['energy'])
             forces.append(trajectory.frame_properties[frame_id]['forces'])
