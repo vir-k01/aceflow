fac6ee124e0367268585ff7a2d1e1f96f1a35083
Vir Karan <virkaran123456@gmail.com>
2024-07-04 15:12:45 -0700

    fixed type

--- src/ace_jobflow/jobs/train.py
+++ src/ace_jobflow/jobs/train.py
@@ -4,7 +4,7 @@ import subprocess
 from ..utils.util import write_input
 
 @job
-def naive_train_ACE(computed_data_set = None, pre_computed_dataset : pd.DataFrame | None = None, num_basis : int = 10, cutoff : int = 7, loss_weight : float = 0.9, max_steps: int = 2000, batch_size: int = 200, gpu_index: int | None = None):
+def naive_train_ACE(computed_data_set = None, pre_computed_dataset : pd.DataFrame = None, num_basis : int = 10, cutoff : int = 7, loss_weight : float = 0.9, max_steps: int = 2000, batch_size: int = 200, gpu_index: int = None):
     computed_data_set = pd.DataFrame.from_dict(computed_data_set)
     data_set = pd.concat([computed_data_set, pre_computed_dataset], axis=0, join="outer", ignore_index=False, keys=None)
     data_set.to_pickle("data.pckl.gzip", compression='gzip', protocol=4)
