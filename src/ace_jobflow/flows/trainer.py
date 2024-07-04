5d8831f272294fcae41dfb864e9abb45ce44f981
Vir Karan <virkaran123456@gmail.com>
2024-07-04 15:24:50 -0700

    fixed naive_flow

--- src/ace_jobflow/flows/trainer.py
+++ src/ace_jobflow/flows/trainer.py
@@ -7,5 +7,5 @@ from ace_jobflow.jobs.train import naive_train_ACE
 def naive_flow(compositions: list, num_points: int = 5, temperature: float = 2000, max_steps: int = 2000, batch_size: int = 200, gpu_index: int = None):
     data = data_gen_flow(compositions, num_points=num_points, temperature=temperature)
     read_job = read_outputs(data.output)
-    trainer = naive_flow(read_job.output, max_steps=max_steps, batch_size=batch_size, gpu_index=gpu_index)
+    trainer = naive_train_ACE(read_job.output, max_steps=max_steps, batch_size=batch_size, gpu_index=gpu_index)
     return Flow([data, read_job, trainer], output=trainer.output, name='ACE_wf_Test')
