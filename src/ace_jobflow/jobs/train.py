5918586259d7124456382adaf295062742c86b28
Vir Karan <virkaran123456@gmail.com>
2024-07-05 11:24:26 -0700

    ace trainer returns cwd for next trainer to pick up

--- src/ace_jobflow/jobs/train.py
+++ src/ace_jobflow/jobs/train.py
@@ -2,9 +2,10 @@ from jobflow import job
 import pandas as pd
 import subprocess
 from ..utils.util import write_input
+import os
 
 @job
-def naive_train_ACE(computed_data_set = None, pre_computed_dataset : pd.DataFrame = None, num_basis : int = 10, cutoff : int = 7, loss_weight : float = 0.9, max_steps: int = 2000, batch_size: int = 200, gpu_index: int = None, prev_dir: str = None):
+def naive_train_ACE(computed_data_set = None, pre_computed_dataset : pd.DataFrame = None, num_basis : int = 10, cutoff : int = 7, loss_weight : float = 0.9, max_steps: int = 2000, batch_size: int = 200, gpu_index: int = None, prev_dir: str = None) -> str:
     computed_data_set = pd.DataFrame.from_dict(computed_data_set)
     data_set = pd.concat([computed_data_set, pre_computed_dataset], axis=0, join="outer", ignore_index=False, keys=None)
     write_input(num_basis, cutoff, loss_weight, max_steps, batch_size, gpu_index)
@@ -20,3 +21,4 @@ def naive_train_ACE(computed_data_set = None, pre_computed_dataset : pd.DataFram
         data_set.to_pickle("data.pckl.gzip", compression='gzip', protocol=4)
         subprocess.run("pacemaker input.yaml", shell=True)
         #subprocess.run("pace_activeset -d fitting_data_info.pckl.gzip output_potential.yaml")
+    return os.getcwd()
