19caca117018214c41e725e8b80744aa663838a6
Vir Karan <virkaran123456@gmail.com>
2024-07-05 11:39:42 -0700

    ace trainer returns cwd for next trainer to pick up

--- src/ace_jobflow/jobs/train.py
+++ src/ace_jobflow/jobs/train.py
@@ -9,16 +9,15 @@ def naive_train_ACE(computed_data_set = None, pre_computed_dataset : pd.DataFram
     computed_data_set = pd.DataFrame.from_dict(computed_data_set)
     data_set = pd.concat([computed_data_set, pre_computed_dataset], axis=0, join="outer", ignore_index=False, keys=None)
     write_input(num_basis, cutoff, loss_weight, max_steps, batch_size, gpu_index)
+    data_set.to_pickle("data.pckl.gzip", compression='gzip', protocol=4)
     if prev_dir is not None:
-        data_set.to_pickle(prev_dir + "/data.pckl.gzip", compression='gzip', protocol=4)
+        #data_set.to_pickle(prev_dir + "/data.pckl.gzip", compression='gzip', protocol=4)
         try:
             subprocess.run("cp " + prev_dir + "/output_potential.yaml ./continue.yaml", shell=True)
         except:
-            subprocess.run("cp " + prev_dir + "/interim_potential.yaml ./continue.yaml", shell=True)
-        
+            subprocess.run("cp " + prev_dir + "/interim_potential_0.yaml ./continue.yaml", shell=True)
         subprocess.run("pacemaker -p continue.yaml input.yaml", shell=True)
     else:
-        data_set.to_pickle("data.pckl.gzip", compression='gzip', protocol=4)
         subprocess.run("pacemaker input.yaml", shell=True)
         #subprocess.run("pace_activeset -d fitting_data_info.pckl.gzip output_potential.yaml")
     return os.getcwd()
