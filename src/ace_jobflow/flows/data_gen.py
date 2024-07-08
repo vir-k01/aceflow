6f317018d36a6cff6e26b19e765fae6e50342f46
Vir Karan <virkaran123456@gmail.com>
2024-07-08 11:38:09 -0700

    Test with ACE-mD

--- src/ace_jobflow/flows/data_gen.py
+++ src/ace_jobflow/flows/data_gen.py
@@ -22,14 +22,14 @@ def data_gen_flow(compositions: list = None, md_maker =  None, num_points: int =
         for composition in compositions:
             working_structures.append(get_random_packed(composition))
     if md_maker is None:
-        '''md_maker = PyACEMDMaker(**{"time_step": 2,
+        md_maker = PyACEMDMaker(**{"time_step": 2,
                         "n_steps": 10,
                         "temperature": temperature,
                         "calculator_kwargs": {'basis_set':'/pscratch/sd/v/virkaran/ace_test/train/29-6/2/output_potential.yaml'},
                         "traj_file": "test-ACE.traj",
                         "traj_file_fmt": "pmg",
                         "traj_interval": 1
-    })'''
+    })
         md_maker = MDMaker(temperature=temperature, end_temp=temperature, steps=200)
     
     linear_strain = np.linspace(-0.2, 0.2, num_points)
