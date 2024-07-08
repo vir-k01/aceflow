89b27ee10376336ee5f34a952fe5ebdca7d4667c
Vir Karan <virkaran123456@gmail.com>
2024-07-08 11:35:35 -0700

    Added N step trainer

--- src/ace_jobflow/flows/trainer.py
+++ src/ace_jobflow/flows/trainer.py
@@ -28,13 +28,13 @@ class NaiveACEFlowMaker(Maker):
             read_job = read_outputs(precomputed_dataset=precomputed_data, step_skip=self.step_skip)
             trainer = naive_train_ACE(read_job.output, max_steps=self.max_steps, batch_size=self.batch_size, gpu_index=self.gpu_index)
             train_checker = check_training_output(trainer.output)
-            return Flow([read_job, trainer, train_checker], output=train_checker.output, name='ACE_wf_Test')
+            return Flow([read_job, trainer, train_checker], output=train_checker.output, name=self.name)
         else: 
             data = data_gen_flow(compositions, num_points=self.num_points, temperature=self.temperature, md_maker=self.md_maker, structures=structures)
             read_job = read_outputs(md_outputs = data.output, precomputed_dataset=precomputed_data, step_skip=self.step_skip)
             trainer = naive_train_ACE(read_job.output, max_steps=self.max_steps, batch_size=self.batch_size, gpu_index=self.gpu_index)
             train_checker = check_training_output(trainer.output)
-            return Flow([data, read_job, trainer, train_checker], output=train_checker.output, name='ACE_wf_Test')
+            return Flow([data, read_job, trainer, train_checker], output=train_checker.output, name=self.name)
         
 @dataclass
 class NaiveACETwoStepFlowMaker(NaiveACEFlowMaker):
@@ -52,7 +52,7 @@ class NaiveACETwoStepFlowMaker(NaiveACEFlowMaker):
             train_checker_1 = check_training_output(train_step_1.output)
             train_step_2 = naive_train_ACE(read_job.output, loss_weight=self.loss_weights[1], prev_run_dict=train_checker_1.output, max_steps=self.max_steps, batch_size=self.batch_size, gpu_index=self.gpu_index)
             train_checker_2 = check_training_output(train_step_2.output)
-            return Flow([read_job, train_step_1, train_checker_1, train_step_2, train_checker_2], output=train_checker_2.output, name='ACE_wf_Test')
+            return Flow([read_job, train_step_1, train_checker_1, train_step_2, train_checker_2], output=train_checker_2.output, name=self.name)
         else: 
             data = data_gen_flow(compositions, num_points=self.num_points, temperature=self.temperature, md_maker=self.md_maker, structures=structures)
             read_job = read_outputs(md_outputs= data.output, precomputed_dataset=precomputed_data, step_skip=self.step_skip)
@@ -60,7 +60,37 @@ class NaiveACETwoStepFlowMaker(NaiveACEFlowMaker):
             train_checker_1 = check_training_output(train_step_1.output)
             train_step_2 = naive_train_ACE(read_job.output, loss_weight=self.loss_weights[1], prev_run_dict=train_checker_1.output, max_steps=self.max_steps, batch_size=self.batch_size, gpu_index=self.gpu_index)
             train_checker_2 = check_training_output(train_step_2.output)
-            return Flow([read_job, train_step_1, train_checker_1, train_step_2, train_checker_2], output=train_checker_2.output, name='ACE_wf_Test')
+            return Flow([data, read_job, train_step_1, train_checker_1, train_step_2, train_checker_2], output=train_checker_2.output, name=self.name)
+
+@dataclass
+class NaiveACENStepFlowMaker(NaiveACEFlowMaker):
+    '''
+    N Step ACE Trainer: same as above, but now does the training in N steps: first with emphasis on forces (kappa=0.99), followed by progressively increasing focus on energies.
+    The flow returns the directory with the output_potential.yaml, training log and reports.
+    '''
+    name : str = 'Naive ACE N-Step Trainer'
+    loss_weights = [0.99, 0.9, 0.3]
+
+    def make(self, compositions: list = None, precomputed_data : pd.DataFrame = None, structures: list = None):
+        if compositions is None and structures is None:
+            read_job = read_outputs(precomputed_dataset=precomputed_data, step_skip=self.step_skip)
+            train_steps = []
+            train_checkers = []
+            for i in range(len(self.loss_weights)):
+                train_steps.append(naive_train_ACE(read_job.output, loss_weight=self.loss_weights[i], max_steps=self.max_steps, batch_size=self.batch_size, gpu_index=self.gpu_index))
+                train_checkers.append(check_training_output(train_steps[-1].output))
+            return Flow([read_job, *train_steps, *train_checkers], output=train_checkers[-1].output, name=self.name)
+        else: 
+            data = data_gen_flow(compositions, num_points=self.num_points, temperature=self.temperature, md_maker=self.md_maker, structures=structures)
+            read_job = read_outputs(md_outputs=data.output, precomputed_dataset=precomputed_data, step_skip=self.step_skip)
+            train_steps = []
+            train_checkers = []
+            for i in range(len(self.loss_weights)):
+                train_steps.append(naive_train_ACE(read_job.output, loss_weight=self.loss_weights[i], max_steps=self.max_steps, batch_size=self.batch_size, gpu_index=self.gpu_index))
+                train_checkers.append(check_training_output(train_steps[-1].output))
+            return Flow([data, read_job, *train_steps, *train_checkers], output=train_checkers[-1].output, name=self.name)
+
+
 
 '''
 def naive_flow(compositions: list, num_points: int = 5, temperature: float = 2000, max_steps: int = 2000, batch_size: int = 200, gpu_index: int = None):
