d9859ec3e8e59933f1ef8b3efbc5cde5a75745ea
Vir Karan <virkaran123456@gmail.com>
2024-07-08 12:00:19 -0700

    updated init

--- src/ace_jobflow/__init__.py
+++ src/ace_jobflow/__init__.py
@@ -2,5 +2,6 @@
 from ace_jobflow.jobs.train import naive_train_ACE
 from ace_jobflow.jobs.data import read_outputs
 from ace_jobflow.utils.util import write_input
+from ace_jobflow.utils.active_set_generator import get_active_set
 from ace_jobflow.flows.data_gen import data_gen_flow
 from ace_jobflow.flows.trainer import NaiveACEFlowMaker, NaiveACETwoStepFlowMaker
