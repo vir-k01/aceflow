2cb6806c2748f2b325c5616da38e5eb8e8538c57
Vir Karan <virkaran123456@gmail.com>
2024-07-09 16:37:22 -0700

    fixed write input

--- src/aceflow/__init__.py
+++ src/aceflow/__init__.py
@@ -6,4 +6,4 @@ from aceflow.utils.structure_sampler import generate_test_points
 from aceflow.utils.active_learning import get_active_set, select_structures_with_active_set
 from aceflow.flows.data import DataGenFlowMaker, ActiveStructuresFlowMaker
 from aceflow.flows.trainer import NaiveACEFlowMaker, NaiveACETwoStepFlowMaker, ACEMaker
-from aceflow.utils.config import TrainConfig, DataConfig, ActiveLearningConfig
+from aceflow.utils.config import TrainConfig, DataGenConfig, ActiveLearningConfig
