78e94df7f6ebb614f5b24e9b5aec06e9ef02aee3
Vir Karan <virkaran123456@gmail.com>
2024-07-10 14:10:15 -0700

    check issue with active_loops, made naive_train take in dict or df, added consolidate_data jobs

--- src/aceflow/__init__.py
+++ src/aceflow/__init__.py
@@ -1,5 +1,5 @@
 from aceflow.jobs.train import naive_train_ACE
-from aceflow.jobs.data import read_MD_outputs, test_potential_in_restricted_space
+from aceflow.jobs.data import read_MD_outputs, test_potential_in_restricted_space, consolidate_data
 from aceflow.utils.util import write_input
 from aceflow.utils.structure_sampler import generate_test_points
 from aceflow.utils.active_learning import get_active_set, select_structures_with_active_set
