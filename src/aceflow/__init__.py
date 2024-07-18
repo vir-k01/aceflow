59e0286d9c7ce1ba187656192c92e5dbf8dbb1a5
Vir Karan <virkaran123456@gmail.com>
2024-07-18 09:39:35 -0700

    remove old flows from init

--- src/aceflow/__init__.py
+++ src/aceflow/__init__.py
@@ -4,5 +4,5 @@ from aceflow.utils.util import write_input
 from aceflow.utils.structure_sampler import generate_test_points
 from aceflow.utils.active_learning import get_active_set, select_structures_with_active_set
 from aceflow.flows.data import DataGenFlowMaker, ActiveStructuresFlowMaker
-from aceflow.flows.trainer import NaiveACEFlowMaker, NaiveACENStepFlowMaker, ACEMaker, ProductionACEMaker
+from aceflow.flows.trainer import ProductionACEMaker
 from aceflow.utils.config import TrainConfig, DataGenConfig, ActiveLearningConfig
