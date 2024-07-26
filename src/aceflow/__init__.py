b47806761970f0b43cd0fc8872393df68727abd8
Vir Karan <virkaran123456@gmail.com>
2024-07-26 14:15:52 -0700

    update init.py

--- src/aceflow/__init__.py
+++ src/aceflow/__init__.py
@@ -1,11 +1,12 @@
 from aceflow.jobs.train import naive_train_ACE, naive_train_hACE
 from aceflow.jobs.data import read_MD_outputs, test_potential_in_restricted_space, consolidate_data
 from aceflow.utils.input_writer import write_input, flexible_input_writer
-from aceflow.utils.structure_sampler import generate_test_points
+from aceflow.utils.structure_sampler import get_random_packed_points
 from aceflow.active_learning.active_learning import get_active_set, select_structures_with_active_set
 from aceflow.reference_objects.BBasis_classes import FlowBBasisOrder, UnaryBBasisOrder, BinaryBBasisOrder, TernaryBBasisOrder, QuaternaryBBasisOrder, QuinaryBBasisOrder, AllBBasisOrder, BBasisBonds, BBasisEmbedding
 from aceflow.flows.data import DataGenFlowMaker, ActiveStructuresFlowMaker
 from aceflow.flows.trainer import ProductionACEMaker, ACEMaker, HeirarchicalACEMaker
 from aceflow.utils.config import TrainConfig, DataGenConfig, ActiveLearningConfig, HeirarchicalFitConfig
+from aceflow.active_learning.core import RandomPackedSampler, HighTempMDSampler, RandomPackedSampler, HighTempMDSampler
 from aceflow.core.model import TrainedPotential
 from aceflow.schemas.core import ACETrainerTaskDoc, ACEDataTaskDoc
